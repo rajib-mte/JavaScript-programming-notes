13537126a9093cc16e55994a99c9af78879ec10e
Rajib Hasan <rajibhasan.mte@gmail.com>
2026-01-17 14:46:51 +0600

    added: basics program

---
+++ 0.basics.js
@@ -0,0 +1,32 @@
+console.log("Hello, World!");
+let a = 10;
+let b = 20;
+let sum = a + b;
+
+console.log("Sum =", sum);
+let num = 7;
+
+if (num % 2 === 0) {
+  console.log("Even");
+} else {
+  console.log("Odd");
+}
+let x = 15;
+let y = 25;
+
+if (x > y) {
+  console.log("x is larger");
+} else {
+  console.log("y is larger");
+}
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+}
+let n = 5;
+let fact = 1;
+
+for (let i = 1; i <= n; i++) {
+  fact *= i;
+}
+
+console.log("Factorial =", fact);
